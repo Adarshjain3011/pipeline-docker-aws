17279fadff70bb4cc3294e8acdc95fc09f5646e4
jainArchit1 <architsupper@gmail.com>
2024-10-05 09:53:58 +0530

    done

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-
-    res.send("Hello World!");
-
-})
-
-
-app.get("/user",(req,res)=>{
-
-    res.json({name: "John Doe", age: 30});
-
-})
-
-app.post("/user",(req,res)=>{
-
-    res.json(req.body);
-
-    console.log(req.body);
-
-    res.status(201).json({
-
-        message:"data updated successfully",
-
-    });
-})
-
-app.get("/count",(req,res)=>{
-
-    res.json({count: 100});
-    
-})
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+app.get("/user", (req, res) => {
+  res.json({ name: "John Doe", age: 30 });
+});
+
+app.post("/user", (req, res) => {
+  res.json(req.body);
+
+  console.log(req.body);
+
+  res.status(201).json({
+    message: "data updated successfully",
+  });
+});
+
+app.get("/count", (req, res) => {
+  res.json({ count: 100 });
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 3000");
+});
